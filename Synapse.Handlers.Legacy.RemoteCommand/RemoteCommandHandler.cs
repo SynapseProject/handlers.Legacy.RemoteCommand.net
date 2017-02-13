1a455dcec10fafa2a5a8d226a494417bb870adba
Guy Waguespack <guy.waguespack@bp.com>
2017-02-13 14:59:24 -0600

    Removed Reduntant "Complete" update.

--- Synapse.Handlers.Legacy.RemoteCommand/RemoteCommandHandler.cs
+++ Synapse.Handlers.Legacy.RemoteCommand/RemoteCommandHandler.cs
@@ -35,7 +35,6 @@ public class RemoteCommandHandler : HandlerRuntimeBase
         seqNo = 0;
         OnProgress("Execute", "Starting", StatusType.Running, startInfo.InstanceId, seqNo++);
         wf.ExecuteAction(startInfo.IsDryRun);
-        OnProgress("Execute", "Completed", StatusType.Complete, startInfo.InstanceId, seqNo++);
 
         return new ExecuteResult() { Status = StatusType.Complete };
     }
